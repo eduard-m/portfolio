e82b6ac0f5e537f591d2ab4a2ac54ec73b21896f
Eduard <ediqediq@gmail.com>
2020-05-30 12:54:46 +0300

    First commit

---
+++ docs/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+    const mobileMenuToggle = document.querySelector('.toggle-menu');
+    const mobMenu = document.querySelector('.header__menu');
+    const overlay = document.querySelector('#overlay');
+
+    mobileMenuToggle.addEventListener('click', function(){
+        mobMenu.classList.toggle('active');
+        this.classList.toggle('active');
+        overlay.classList.toggle('active');
+
+    });
+
+    window .addEventListener('resize', function(){
+        mobMenu.classList.remove('active');
+        mobileMenuToggle.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
+
+    // placeholder
+
+    const formInputs = document.querySelectorAll('.form-item');
+
+    for(let item of formInputs){
+
+        const inputPlaceholder = item.nextElementSibling;
+
+        item.addEventListener('click', function() {
+            inputPlaceholder.classList.add('active');
+        });
+
+        item.addEventListener('blur', function(){
+            if(this.value == ''){
+                inputPlaceholder.classList.remove('active');
+            }
+        })
+    }
+
+    let containerEl = document.querySelector('#portfolio-projects');
+
+    let mixer = mixitup(containerEl);
+})
+
+
+
