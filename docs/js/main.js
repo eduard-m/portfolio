2232b3a854d3e1cf07456bc464b5feda2b57dc4e
Eduard <ediqediq@gmail.com>
2020-05-30 14:13:34 +0300

    Third commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -17,6 +17,21 @@ $(document).ready(function() {
         overlay.classList.remove('active');
     });
 
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+
 
     // placeholder
 
